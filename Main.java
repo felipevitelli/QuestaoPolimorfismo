ea00cdfc0cb5c7cd340c0528fdef21d02f8717b5
felipevitelli <142325386+felipevitelli@users.noreply.github.com>
2024-11-08 14:46:40 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         Funcionario[] funcionarios = {
                 new Funcionario("João", 3000),
                 new Gerente("Maria", 5000, 2000),
-                new Diretor("Carlos", 7000, 5000)
+                new Diretor("Felipe", 7000, 5000)
         };
 
         FolhaPagamento folha = new FolhaPagamento();
